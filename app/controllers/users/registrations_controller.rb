755845023c59d3873b230620d9a668de1cbec197
James Hibbeard <jhibbeard@gmail.com>
2021-11-16 11:11:24 +0100

    Added first and last name to user db, created additions to devise and serializer to return all values to clients

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, if: :devise_controller?
   respond_to :json
 
   private
@@ -56,10 +57,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # def user_params
+  #   params.require(:user).permit(:first_name, :last_name, :email, :password)
   # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :first_name, :last_name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valud with valid attributes' do
-    user = User.create({ email: 'jhibbeard@example.com', password: '123456' })
+  it 'is valid with valid attributes' do
+    user = User.create({ first_name: 'James', last_name: 'Hibbeard', email: 'jhibbeard@example.com', password: '123456' })
     expect(user).to be_valid
   end
   it 'is not valid without an email' do
