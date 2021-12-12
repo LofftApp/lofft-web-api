7feb4ba5c9b3fc59444ac35bc1f878d571841c30
James Hibbeard <jhibbeard@gmail.com>
2021-12-12 17:18:55 +0100

    Added bills seed and changed value column from int to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_153357) do
+ActiveRecord::Schema.define(version: 2021_12_12_161547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_153357) do
   create_table "bills", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "value"
+    t.float "value"
     t.string "currency", default: "EUR"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -28,4 +28,23 @@ apartments.each do |apartment|
   Apartment.create(apartment)
 end
 
+# Bills
+
+bills = [
+  {name: "Beers", description: "Beers at the pub", value: 125 },
+  {name: "Washing Machine", description: "New Washing Machine in the WG", value: 450 },
+  {name: "IOU", description: "Money I borrowed from last week", value: 4.50 },
+  {name: "Cleaning Bill", description: "Cleaning of apartment from Party", value: 150 },
+  {name: "Vinyl Player", description: "Our new Bougy AF Vinyl Player", value: 85 },
+  {name: "Lebensmittle", description: "Lebensmittle für Mittwoch Abendessen", value: 21 },
+  {name: "Party Beers", description: "Beers and food for last weeks party", value: 212 },
+  {name: "New WG Bike", description: "The new WG Bike", value: 32 },
+  {name: "Lavée notre appartement", description: "Que j'ecries", value: 85 }
+]
+
+bills.each do |bill|
+  Bill.create(bill)
+end
+
+
 p "Seeding complete"
