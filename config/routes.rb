7428cf82ea5ec7bab2f5bab524ccdfb7f31de80f
James Hibbeard <jhibbeard@gmail.com>
2022-01-02 11:34:25 +0100

    Updated routes comply with new object interaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Added due to failing test requiring hosts to exist change to production URL.
-  default_url_options :host => "lofft.app"
+  default_url_options host: 'lofft.app'
   namespace :api, defaults: { format: :json } do
     resources :users, only: %w[show]
     resources :apartments, only: %w[index create]
