44c438d82b53bc903da6216f13d40e2c37524452
James Hibbeard <jhibbeard@gmail.com>
2022-01-03 21:28:22 +0100

    Update to returned results

--- app/controllers/api/user_bills_controller.rb
+++ app/controllers/api/user_bills_controller.rb
@@ -1,7 +1,30 @@
 class Api::UserBillsController < ApplicationController
   def index
-    bills = UserBill.where(user_id: current_user.id)
-    render json: bills
+    results = []
+    user_bills = UserBill.where(user_id: current_user.id)
+    user_bills.each do |user_bill|
+      details = {
+        id: user_bill.id,
+        user_id: user_bill.user_id,
+        item_value: user_bill.value,
+        accepted: user_bill.accepted,
+        paid: user_bill.paid,
+        user: {
+          id: user_bill.user.id,
+          first_name: user_bill.user.first_name,
+          last_name: user_bill.user.last_name
+        },
+        bill: {
+          id: user_bill.bill.id,
+          name: user_bill.bill.name,
+          description: user_bill.bill.description,
+          value: user_bill.bill.value,
+          currency: user_bill.bill.currency
+        }
+      }
+      results << details
+    end
+    render json: results
   end
 
   def update
